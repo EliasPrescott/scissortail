754b38279ff1080b2b78c5c9066ae0259c973769
eliasprescott <syzygyprescott@gmail.com>
2026-01-04 17:26:51 -0600

    cleaning up flake and libtailscale packaging
    The class library still is not ready to be packaged as a standalone
    Nuget package, but this should lay the groundwork for that in the future
    if I want to try for that.

--- flake.nix
+++ flake.nix
@@ -8,52 +8,55 @@
   };
 
   outputs = { self, nixpkgs, libtailscale }: let
-    pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-  in rec {
-    # Reference: https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/dotnet.section.md
-    packages.aarch64-darwin.class-library = pkgs.buildDotnetModule {
-      pname = "Scissortail";
-      version = "0.0.1";
-      src = ./Scissortail;
-      projectFile = "Scissortail.csproj";
-      nugetDeps = ./Scissortail/deps.json;
-      packNupkg = true;
-      dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
-      NIX_LIBS = "
-        ${libtailscale.packages.aarch64-darwin.default}/bin/libtailscale
-      ";
-    };
+    lib = nixpkgs.lib;
+    defaultSystems = [
+      "aarch64-darwin"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "x86_64-linux"
+    ];
+    eachDefaultSystem = lib.genAttrs defaultSystems;
+  in {
+    packages = eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in rec {
+      # https://learn.microsoft.com/en-us/nuget/create-packages/native-files-in-net-packages
+      # Reference: https://github.com/NixOS/nixpkgs/blob/master/doc/languages-frameworks/dotnet.section.md
+      class-library = pkgs.buildDotnetModule {
+        pname = "Scissortail";
+        version = "0.0.1";
+        src = ./Scissortail;
+        projectFile = "Scissortail.csproj";
+        nugetDeps = ./Scissortail/deps.json;
+        packNupkg = true;
+        dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
+        SCISSORTAIL_LIBTAILSCALE = "${libtailscale.packages.${system}.default}/bin/libtailscale";
+      };
 
-    packages.aarch64-darwin.default = pkgs.buildDotnetModule {
-      pname = "Scissortail.MvcExample";
-      version = "0.0.1";
-      src = ./Scissortail.MvcExample;
-      projectFile = "Scissortail.MvcExample.csproj";
-      buildInputs = [ packages.aarch64-darwin.class-library ];
-      nugetDeps = ./Scissortail.MvcExample/deps.json;
-      dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
-      dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_9_0;
-      executables = [ "Scissortail.MvcExample" ];
-      NIX_LIBS = "
-        ${libtailscale.packages.aarch64-darwin.default}/bin/libtailscale
-      ";
-      runtimeDeps = [
-        "${libtailscale.packages.aarch64-darwin.default}/bin/libtailscale"
-      ];
-      # https://discourse.nixos.org/t/building-asp-net-core-app-with-nix-serving-static-files/30810/4
-      makeWrapperArgs = [
-        "--set DOTNET_CONTENTROOT ${placeholder "out"}/lib/Scissortail.MvcExample"
-      ];
-    };
+      default = pkgs.buildDotnetModule {
+        pname = "Scissortail.MvcExample";
+        version = "0.0.1";
+        src = ./Scissortail.MvcExample;
+        projectFile = "Scissortail.MvcExample.csproj";
+        buildInputs = [ class-library ];
+        nugetDeps = ./Scissortail.MvcExample/deps.json;
+        dotnet-sdk = pkgs.dotnetCorePackages.sdk_9_0;
+        dotnet-runtime = pkgs.dotnetCorePackages.aspnetcore_9_0;
+        executables = [ "Scissortail.MvcExample" ];
+        # https://discourse.nixos.org/t/building-asp-net-core-app-with-nix-serving-static-files/30810/4
+        makeWrapperArgs = [
+          "--set DOTNET_CONTENTROOT ${placeholder "out"}/lib/Scissortail.MvcExample"
+        ];
+      };
 
-    devShells.aarch64-darwin.default = pkgs.mkShell {
-      name = "scissortail";
-      nativeBuildInputs = [
-        pkgs.pkg-config
-      ];
-      NIX_LIBS = "
-        ${libtailscale.packages.aarch64-darwin.default}/bin
-      ";
-    };
+      container-image = pkgs.dockerTools.buildImage {
+        name = "scissortail-mvc-example";
+        tag = "0.0.1";
+        config = {
+          Cmd = [ "${default}/bin/Scissortail.MvcExample" ];
+        };
+        created = "now";
+      };
+    });
   };
 }
